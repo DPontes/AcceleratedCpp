bae71c5a392f39d0ef5edbe59ab277c4fa02c12a
DPontes <dpontes@protonmail.com>
2019-09-28 21:47:00 +0200

    Works

---
+++ Chapter1-Working_with_strings/helloperson.cpp
@@ -0,0 +1,16 @@
+// Ask for a person's name and greet that person
+#include <iostream>
+#include <string>
+
+int main() {
+    // ask for the person's name
+    std::cout << "Please enter your first name: " ;
+
+    // read the name
+    std::string name;
+    std::cin >> name;
+
+    // Write a greeting
+    std::cout << "Hello, " << name << "!" << std::endl;
+    return 0;
+}
