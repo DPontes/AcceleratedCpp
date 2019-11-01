577be79705eb9b5a150ed15a58e70e33380f38e3
DPontes <dpontes@protonmail.com>
2019-11-02 00:14:38 +0100

    Function to calculate student's grade in single file, separate functions

--- Ch4-Organizing_Programs_and_Data/4.1-OrganizingComputations.cpp
+++ Ch4-Organizing_Programs_and_Data/4.1-OrganizingComputations.cpp
@@ -10,6 +10,7 @@ using std::cin;     using std::setprecision;
 using std::cout;    using std::streamsize;
 using std::sort;    using std::string;
 using std::endl;    using std::vector;
+using std::istream; using std::domain_error;
 
 // Compute a student's overall grade from midterm
 // and final exam grades and homework
@@ -24,7 +25,7 @@ double median(vector<double> vec) {
 
     vector_size size = vec.size();
     if (size == 0) throw domain_error("Median of an empty vector");
-    sort(vect.begin(), vec.end());
+    sort(vec.begin(), vec.end());
     vector_size mid = size / 2;
     return size % 2 == 0 ? (vec[mid] + vec[mid-1]) / 2 : vec[mid];
 }
@@ -35,4 +36,54 @@ double median(vector<double> vec) {
 double grade(double midterm, double final, const vector<double>& hw) {
     if (hw.size() == 0) throw domain_error("Student has done no homework");
     return grade (midterm, final, median(hw));
+}
+
+// read homework grades from an input stream into a vector<double>
+istream& read_hw(istream& in, vector<double>& hw) {
+    if (in) {
+        // get rid of previous content
+        hw.clear();
+
+        // read homework grades
+        double x;
+        while (in >> x) hw.push_back(x);
+
+        // clear the stream so that input will work for the next student
+        in.clear();
+    }
+    return in;
+}
+
+int main() {
+    // ask for and read the student's name
+    cout << "Please enter your first name: ";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+
+    // ask for and read the midterm and final grades
+    cout << "Please enter your midterm and final exam grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
+
+    // ask for homework grades
+    cout << "Please enter all your homework grades, followed by EoF";
+    cout << " (Win Ctrl+Z / Unix: Ctrl+D): ";
+
+    vector<double> homework;
+
+    // read the homework grades
+    read_hw(cin, homework);
+
+    // compute and generate the final grade, if possible
+    try {
+        double final_grade = grade(midterm, final, homework);
+        streamsize prec = cout.precision();
+        cout << "Your final grade is " << setprecision(3)
+             << final_grade << setprecision(prec) << endl;
+    } catch (domain_error) {
+        cout << endl << "You must enter your grades. Please try again." << endl;
+        return 1;
+    }
+    return 0;
 }
